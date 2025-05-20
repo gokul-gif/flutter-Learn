12a97eacd151084bd6fbb55dd7b580021558f663
GOKUL <gokulsunil036@gmail.com>
2025-05-20 15:14:19 +0530

    for loop

--- learn1/bin/learn1.dart
+++ learn1/bin/learn1.dart
@@ -1,15 +1,7 @@
-import 'dart:io';
+//for loop
 
 void main() {
-  var a = stdin.readLineSync();
-
-  var b = int.parse(a!);
-
-  if (b > 0) {
-    print('Positive');
-  } else if (b < 0) {
-    print('Negative');
-  } else {
-    print('Zero');
+  for (var i = 0; i < 11; i++) {
+    print(i);
   }
 }
