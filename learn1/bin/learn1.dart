a16eb34c82afece97ee578903898cfb3a0a1a88e
GOKUL <gokulsunil036@gmail.com>
2025-05-20 15:07:32 +0530

    data type

--- learn1/bin/learn1.dart
+++ learn1/bin/learn1.dart
@@ -1,5 +1,16 @@
-import 'package:learn1/learn1.dart' as learn1;
+void main() {
+  int a = 10; //store integer
+  double b = 20; //store double
+  num n1 = 110; //store int or double
+  num n2 = 1100.110;
 
-void main(List<String> arguments) {
-  print('Hello world: ${learn1.calculate()}!');
+  String s1 = 'signle line';
+  String s2 = "multivalued and can use special charectors(i'am gokul)";
+  String s3 = '''this is multiline
+  no error for multli line
+  can handle infinite number of lines''';
+
+  //string opertations
+
+  print(s1.length);
 }
