47fa4b662ed54685fb67b98cb34416b0057b7495
GOKUL <gokulsunil036@gmail.com>
2025-05-20 15:12:04 +0530

    if-else-else if

--- learn1/bin/learn1.dart
+++ learn1/bin/learn1.dart
@@ -1,16 +1,15 @@
+import 'dart:io';
+
 void main() {
-  int a = 10; //store integer
-  double b = 20; //store double
-  num n1 = 110; //store int or double
-  num n2 = 1100.110;
+  var a = stdin.readLineSync();
 
-  String s1 = 'signle line';
-  String s2 = "multivalued and can use special charectors(i'am gokul)";
-  String s3 = '''this is multiline
-  no error for multli line
-  can handle infinite number of lines''';
+  var b = int.parse(a!);
 
-  //string opertations
-
-  print(s1.length);
+  if (b > 0) {
+    print('Positive');
+  } else if (b < 0) {
+    print('Negative');
+  } else {
+    print('Zero');
+  }
 }
