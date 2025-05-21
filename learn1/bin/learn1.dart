f8a26c9505d231dda0595fd7d6470eee59af9e37
GOKUL <gokulsunil036@gmail.com>
2025-05-21 17:21:09 +0530

    map

--- learn1/bin/learn1.dart
+++ learn1/bin/learn1.dart
@@ -1,4 +1,8 @@
 void main() {
-  Set<int> set = {1, 1, 1, 2, 3, 4, 0, 0, 0};
-  print(set);
+  Map<String, List<String>> map = {
+    "Name": ["gokul", "gokuls"],
+    "age": ['25', '26']
+  };
+  print(map);
+  print(map['Name']);
 }
